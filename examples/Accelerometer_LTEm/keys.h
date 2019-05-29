8ae94e74066916e18766bb4ced86ae399eaf392c
DESKTOP-P10B2BD\Alain Cuypers <alain@allthingstalk.com>
2019-05-29 13:43:13 +0200

    removed keys

--- examples/Accelerometer_LTEm/keys.h
+++ examples/Accelerometer_LTEm/keys.h
@@ -1,9 +1,10 @@
 #ifndef KEYS_h
 #define KEYS_h
 
-char* APN = "nbiot.iot";
+char* APN = "starter.att.iot";
 char* SPACE = "api.allthingstalk.io";
-char* DEVICE_TOKEN = "maker:4V6dY9RYLbjcW1VeVxawcxwumCqMrfX0cSMm0l5";
-char* DEVICE_ID = "Dn4RnfUgh4na7sV0hxAxjWyj";
+char* DEVICE_ID = "";
+char* DEVICE_TOKEN = "";
+
 
 #endif
--- examples/GPS_LTEm/keys.h
+++ examples/GPS_LTEm/keys.h
@@ -1,9 +1,10 @@
 #ifndef KEYS_h
 #define KEYS_h
 
-char* APN = "nbiot.iot";
+char* APN = "starter.att.iot";
 char* SPACE = "api.allthingstalk.io";
-char* DEVICE_TOKEN = " ";
 char* DEVICE_ID = " ";
+char* DEVICE_TOKEN = " ";
+
 
 #endif
