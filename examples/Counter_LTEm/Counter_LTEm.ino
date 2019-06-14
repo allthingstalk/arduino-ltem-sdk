07e315006e362052871120a23e76e3061eb46c7d
DESKTOP-P10B2BD\Alain Cuypers <alain@allthingstalk.com>
2019-06-14 08:42:37 +0200

    Update Counter_LTEm.ino

--- examples/Counter_LTEm/Counter_LTEm.ino
+++ examples/Counter_LTEm/Counter_LTEm.ino
@@ -1,3 +1,37 @@
+/*    _   _ _ _____ _    _              _____     _ _     ___ ___  _  __
+ *   /_\ | | |_   _| |_ (_)_ _  __ _ __|_   _|_ _| | |__ / __|   \| |/ /
+ *  / _ \| | | | | | ' \| | ' \/ _` (_-< | |/ _` | | / / \__ \ |) | ' <
+ * /_/ \_\_|_| |_| |_||_|_|_||_\__, /__/ |_|\__,_|_|_\_\ |___/___/|_|\_\
+ *                             |___/
+ *
+ * Copyright 2019 AllThingsTalk
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * WHAT DOES THIS SKETCH DO:
+ * -------------------------
+ * Will add 1 to an asset 'counter', if the counter exceeds 100 the counter will
+ * be reset to 1
+ * 
+ * Explanation of the lights:
+ * --------------------------
+ * Blue: initialization with modem and making connection with backend
+ * Magenta: subscribe to message
+ * White: sending data
+ * Green: blinking (initialisation successful), normal (sending successful)
+ * Red: blinking (error initialisation, you have to reset to modem), normal (error in sending)
+ */
+ 
 #include <APICredentials.h>
 #include <CborPayload.h>
 #include <LTEmModem.h>
