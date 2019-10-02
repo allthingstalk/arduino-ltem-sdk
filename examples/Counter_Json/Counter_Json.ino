6babd2da7e39b7131c3c8fe30ad1b9550de227b6
AlainCuypers <39278921+AlainCuypers@users.noreply.github.com>
2019-10-02 07:57:27 +0200

    Update Counter_Json.ino

--- examples/Counter_Json/Counter_Json.ino
+++ examples/Counter_Json/Counter_Json.ino
@@ -37,7 +37,17 @@
 #include "keys.h"
 
 #define debugSerial SerialUSB
-#define ltemSerial SerialSARA
+
+//comment line below if you want to use MKR1500 board
+#define SODAQ_SFF_AFF
+
+#define debugSerial SerialUSB
+
+#ifdef SODAQ_SFF_AFF
+  #define ltemSerial Serial1
+#else
+  #define ltemSerial SerialSARA
+#endif
 
 void callback(const char* data);
 
