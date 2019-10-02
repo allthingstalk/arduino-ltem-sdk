4899c24c6f524146be3e2750be603f63922c45c5
DESKTOP-P10B2BD\Alain Cuypers <alain@allthingstalk.com>
2019-10-02 07:51:46 +0200

    Not for public

--- examples/LTEm_Register_Device/keys.h
+++
@@ -1,8 +0,0 @@
-#ifndef KEYS_h
-#define KEYS_h
-
-// Enter the API endpoint for your Allthingstalk space
-char* SPACE_ENDPOINT = "spicy.allthingstalk.io";
-char* PARTNER_ID = "your partner id";
-
-#endif
