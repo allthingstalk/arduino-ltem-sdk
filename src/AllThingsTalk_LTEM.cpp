26911178c389a5f0efabd8dc73f14c8dcd2bd554
epiller <epiller@users.noreply.github.com>
2020-06-12 15:59:06 +0200

    Initial commit - Compiles but has a lot of stuff left to be done.

---
+++ src/AllThingsTalk_LTEM.cpp
@@ -0,0 +1,226 @@
+#include "AllThingsTalk_LTEM.h"
+#include <ArduinoJson.h>
+
+
+#define URAT            SODAQ_R4X_LTEM_URAT
+#define MNOPROF         MNOProfiles::SIM_ICCID
+#define OPERATOR        AUTOMATIC_OPERATOR
+#define M1_BAND_MASK    BAND_MASK_UNCHANGED
+#define NB1_BAND_MASK   BAND_MASK_UNCHANGED
+
+Sodaq_R4X r4x;
+Sodaq_SARA_R4XX_OnOff saraR4xxOnOff;
+Sodaq_R4X_MQTT r4x_mqtt;
+
+AllThingsTalk_LTEM::AllThingsTalk_LTEM(HardwareSerial &modemSerial, APICredentials &credentials, char* APN) {
+    _modemSerial = &modemSerial;
+    _credentials = &credentials;
+    _APN = APN;
+}
+
+// Serial print (debugging)
+template<typename T> void AllThingsTalk_LTEM::debug(T message, char separator) {
+    if (debugSerial) {
+        debugSerial->print(message);
+        if (separator) {
+            debugSerial->print(separator);
+        }
+    }
+}
+
+// Serial print (verbose debugging)
+template<typename T> void AllThingsTalk_LTEM::debugVerbose(T message, char separator) {
+    if (debugVerboseEnabled) {
+        if (debugSerial) {
+            debugSerial->print(message);
+            if (separator) {
+                debugSerial->print(separator);
+            }
+        }
+    }
+}
+
+void AllThingsTalk_LTEM::debugPort(Stream &debugSerial, bool verbose) {
+    debugVerboseEnabled = verbose;
+    this->debugSerial = &debugSerial;
+    debug("");
+    debug("------------- AllThingsTalk LTE-M Serial Begin -------------");
+    if (!verbose) debug("Debug Level: Normal");
+    debugVerbose("Debug Level: Verbose");
+}
+
+bool AllThingsTalk_LTEM::init() {
+    //TODO: PINS
+    //TODO: LED
+    mqtt.setServer(_credentials->getSpace(), 1883);
+    mqtt.setAuth(_credentials->getDeviceToken(), "arbitrary");
+    // TODO: ADD UNIQUE CLIENT ID GENERATION (MUST!)
+    mqtt.setClientId("UFOHWEUFUEHWOHUFEWfwefwefwe");
+    mqtt.setKeepAlive(300); // Seconds I suppose
+    _modemSerial->begin(r4x.getDefaultBaudrate()); // The transport layer is a Sodaq_R4X
+    r4x.init(&saraR4xxOnOff, *_modemSerial);
+    //r4x.setDiag(DEBUG_STREAM);
+    //r4x_mqtt.setR4Xinstance(&r4x, &AllThingsTalk_LTEM::connectNetwork); // Inform our mqtt instance that we use r4x as the transport
+    //r4x_mqtt.setR4Xinstance(&r4x, std::bind(&AllThingsTalk_LTEM::connectNetwork, this));
+    r4x_mqtt.setR4Xinstance(&r4x, []{ return true; } );
+
+    mqtt.setTransport(&r4x_mqtt);
+    return connect();
+}
+
+bool AllThingsTalk_LTEM::connect() {
+    if (connectNetwork()) {
+        connectMqtt();
+    }
+}
+
+bool AllThingsTalk_LTEM::connectNetwork() {
+    debug("Connecting to Network...");
+    if (r4x.isConnected()) {
+        debug("Already connected to Network!");
+        return true;
+    } else {
+        if (r4x.connect(_APN, URAT, MNOPROF, OPERATOR, M1_BAND_MASK, NB1_BAND_MASK)) {
+            debug("Connected to Network!");
+            return true;
+        } else {
+            debug("Failed to connect to network!");
+            return false;
+        }
+    }
+}
+
+bool AllThingsTalk_LTEM::connectMqtt() {
+    debug("Connecting to MQTT...");
+    if (!r4x_mqtt.isAliveMQTT()) {
+        
+    } else {
+        debug("Already connected to MQTT!");
+    }
+}
+
+bool AllThingsTalk_LTEM::disconnect() {
+    return r4x.disconnect();
+}
+
+bool AllThingsTalk_LTEM::isConnected() {
+    return r4x.isConnected();
+}
+
+bool AllThingsTalk_LTEM::send(Payload &payload) {
+    if (isConnected()) {
+        String rawPayload(payload.getString());
+        int index = rawPayload.indexOf("|"); // Check if it's a JSON
+        bool ok;
+        if (index == -1) { // This is a CBOR Message
+            char* topic;
+            int length = strlen(_credentials->getDeviceId()) + 14;  // 14 fixed chars + deviceId
+            topic = new char[length];
+            sprintf(topic, "device/%s/state", _credentials->getDeviceId());
+            topic[length-1] = 0;
+            return r4x.mqttPublish(topic, payload.getBytes(), payload.getSize(), 0, 0, false);
+            delete(topic);
+        } else {
+            String assetName = rawPayload.substring(0, index);
+            String jsonPayload = rawPayload.substring(index+1);
+            char topic[128];
+            snprintf(topic, sizeof topic, "%s%s%s%s%s", "device/", _credentials->getDeviceId(), "/asset/", assetName.c_str(), "/state");
+            DynamicJsonDocument doc(256);
+            char JSONmessageBuffer[256];
+            doc["value"] = jsonPayload;
+            serializeJson(doc, JSONmessageBuffer);
+            debug("> Message Published to AllThingsTalk (JSON)");
+            debugVerbose("Asset:", ' ');
+            debugVerbose(assetName, ',');
+            debugVerbose(" Value:", ' ');
+            debugVerbose(jsonPayload);
+            return r4x.mqttPublish(topic, string2ByteArray(JSONmessageBuffer), payload.getSize(), 0, 0, false);
+            //return publishMqttMessage(assetName.c_str(), (char*)json.c_str(), true);
+        }
+    } else {
+        debug("Can't send message because you're not connected!");
+        return false;
+    }
+}
+
+BYTE* AllThingsTalk_LTEM::string2ByteArray(char* input) {
+    int loop;
+    int i;
+    BYTE* output;
+    loop = 0;
+    i = 0;
+    while(input[loop] != '\0') {
+        output[i++] = input[loop++];
+    }
+    return output;
+}
+
+
+bool AllThingsTalk_LTEM::setCallback() {
+    // TODO
+}
+
+bool AllThingsTalk_LTEM::registerDevice(const char* deviceSecret, const char* partnerId) {
+  // TO BE IMPLEMENTED
+}
+
+bool AllThingsTalk_LTEM::sendSMS(char* number, char* message) {
+    // TO BE IMPLEMENTED
+}
+
+char* AllThingsTalk_LTEM::getFirmwareVersion() {
+    char versionBuffer[128];
+    if (r4x.getFirmwareVersion(versionBuffer, sizeof(versionBuffer))) {
+        debug("LTE-M Modem Firmware Version: ", ',');
+        debug(versionBuffer);
+        return versionBuffer;
+    } else {
+        debug("Couldn't get the firmware version.");
+        return "ERROR";
+    }
+}
+
+char* AllThingsTalk_LTEM::getIMEI() {
+    char imeiBuffer[128];
+    if (r4x.getIMEI(imeiBuffer, sizeof(imeiBuffer))) {
+        debug("IMEI: ", ',');
+        debug(imeiBuffer);
+        return imeiBuffer;
+    } else {
+        debug("Couldn't get IMEI.");
+        return "ERROR";
+    }
+}
+
+char* AllThingsTalk_LTEM::getOperator() {
+    char operatorBuffer[128];
+    if (r4x.getOperatorInfoString(operatorBuffer, sizeof(operatorBuffer))) {
+        debug("Operator Info: ", ',');
+        debug(operatorBuffer);
+        return operatorBuffer;
+    } else {
+        debug("Couldn't get Operator Info.");
+        return "ERROR";
+    }
+}
+
+bool AllThingsTalk_LTEM::setOperator(const char* apn) {
+    if (r4x.setApn(apn)) {
+        debug("APN Successfully set to: ", ' ');
+        debug(apn);
+        return true;
+    } else {
+        debug("Failed to set APN.");
+        return false;
+    }
+}
+
+void AllThingsTalk_LTEM::reboot() {
+    debug("Rebooting modem...");
+    // Reboot is a private method of R4X. Check if we need it.
+    //r4x.reboot();
+}
+
+void AllThingsTalk_LTEM::loop() {
+    mqtt.loop();
+}
---
+++ src/AllThingsTalk_LTEM.h
@@ -0,0 +1,50 @@
+#ifndef AllThingsTalk_LTEM_h
+#define AllThingsTalk_LTEM_h
+
+#include "Arduino.h"
+#include <Sodaq_R4X.h>
+#include <Sodaq_MQTT.h>
+#include <Sodaq_R4X_MQTT.h>
+#include <Utils.h>
+//#include <String.h>
+//#include <stdio.h>
+#include <ArduinoJson.h>
+#include "APICredentials.h"
+#include "JsonPayload.h"
+// TODO: ABP + DEVICE + OTHERS
+
+typedef unsigned char BYTE;
+
+class AllThingsTalk_LTEM {
+public:
+    AllThingsTalk_LTEM(HardwareSerial &modemSerial, APICredentials &credentials, char* APN = "starter.att.iot");
+    void debugPort(Stream &debugSerial, bool verbose = false);
+    bool init();
+    bool connect();
+    bool disconnect();
+    bool isConnected();
+    bool send(Payload &payload);
+    bool setCallback();
+    bool registerDevice(const char* deviceSecret, const char* partnerId);
+    bool sendSMS(char* number, char* message);
+    char* getFirmwareVersion();
+    char* getIMEI();
+    char* getOperator();
+    bool setOperator(const char* apn);
+    void reboot();
+    void loop();
+
+private:
+    template<typename T> void debug(T message, char separator = '\n');
+    template<typename T> void debugVerbose(T message, char separator = '\n');
+    bool connectNetwork();
+    bool connectMqtt();
+    HardwareSerial *_modemSerial;
+    APICredentials *_credentials;
+    Stream *debugSerial;
+    bool debugVerboseEnabled;
+    char* _APN;
+    BYTE* string2ByteArray(char* input);
+};
+
+#endif
