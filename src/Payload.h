0e0fdc99239b6dbb94cc819788ac9a5813a6c286
epiller <epiller@users.noreply.github.com>
2020-06-19 15:36:25 +0200

    Added missing files from failed merge

---
+++ src/Payload.h
@@ -0,0 +1,12 @@
+#ifndef PAYLOAD_H_
+#define PAYLOAD_H_
+
+class Payload {
+public:
+	virtual char* getString();
+    virtual unsigned char* getBytes() = 0;
+    virtual unsigned int getSize() = 0;
+    virtual void reset() = 0;
+};
+
+#endif
